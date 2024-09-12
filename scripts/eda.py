353c93edd0663d4728f3b58557b1810bb4a800ec
Yonatan Moges <se.yonatan.moges@gmail.com>
2024-09-13 00:49:56 +0300

    Data Quality Assessment

--- scripts/eda.py
+++ scripts/eda.py
@@ -10,3 +10,9 @@ def describe_numerical_columns(df):
 def check_data_types(df):
     """Check the data types of all columns"""
     return df.dtypes
+
+# Data Quality Assessment
+
+def missing_values(df):
+    """Check for missing values in the dataset"""
+    return df.isnull().sum()
