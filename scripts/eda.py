9ce5e40a95767755858c0fcefb9beabcba9c5ca6
Yonatan Moges <se.yonatan.moges@gmail.com>
2024-09-13 00:57:57 +0300

    Univariate Analysis

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,5 +1,5 @@
 
-
+import matplotlib.pyplot as plt
 
 # Data Summarization
 
@@ -16,3 +16,20 @@ def check_data_types(df):
 def missing_values(df):
     """Check for missing values in the dataset"""
     return df.isnull().sum()
+
+# Univariate Analysis
+
+def plot_numerical_histogram(df, columns):
+    """Plot histograms for numerical columns"""
+    df[columns].hist(bins=15, figsize=(15, 10), layout=(len(columns)//4, 4))
+    plt.tight_layout()
+    plt.show()
+
+def plot_categorical_bar(df, columns):
+    """Plot bar charts for categorical columns"""
+    for column in columns:
+        df[column].value_counts().plot(kind='bar', figsize=(10, 5))
+        plt.title(f"Bar plot of {column}")
+        plt.ylabel('Frequency')
+        plt.xlabel(column)
+        plt.show()
